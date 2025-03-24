a7e79c378c6fd5f5a5426bc44ddce00857490190
Lautaro <your.email@example.com>
2025-03-24 20:03:04 -0300

    dsv

--- algo3.hs
+++ algo3.hs
@@ -1,2 +1,5 @@
-sumar::Int->Int->Int
-sumar x y = x+y
+-- practica 0
+--
+--1)
+-- null:: [a]->Bool dada una lista vacia devuelve true, si no, no
+-- head:: [a]->a dada una lista , tail:q, init: ,last: , take: , drop: , ++: , concat: , reverse: , elem: 
